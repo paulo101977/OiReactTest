6af269ca76e98fd9caee9c7901996644652917c0
paulo101977 <paulo10.1977@yahoo.com.br>
2016-11-19 22:16:34 -0200

    First  commit

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const path = require('path');
+const args = require('minimist')(process.argv.slice(2));
+
+// List of allowed environments
+const allowedEnvs = ['dev', 'dist', 'test'];
+
+// Set the correct environment
+let env;
+if (args._.length > 0 && args._.indexOf('start') !== -1) {
+  env = 'test';
+} else if (args.env) {
+  env = args.env;
+} else {
+  env = 'dev';
+}
+process.env.REACT_WEBPACK_ENV = env;
+
+/**
+ * Build the webpack configuration
+ * @param  {String} wantedEnv The wanted environment
+ * @return {Object} Webpack config
+ */
+function buildConfig(wantedEnv) {
+  let isValid = wantedEnv && wantedEnv.length > 0 && allowedEnvs.indexOf(wantedEnv) !== -1;
+  let validEnv = isValid ? wantedEnv : 'dev';
+  let config = require(path.join(__dirname, 'cfg/' + validEnv));
+  
+  
+  config.resolve= {
+    extensions: ['', '.js', '.jsx']
+  }
+  //the configuration below resolves any problem in loaders
+  config.module= {
+    loaders: [
+        {
+            test: /\.jsx?$/,
+            loader: 'babel',
+            exclude: /node_modules/,
+            query: {
+              cacheDirectory: true,
+              presets: ['react', 'es2015']
+            }
+        },
+        { test: /\.css$/, loader: "style-loader!css-loader" },
+        { test: /\.png$/, loader: "url-loader?limit=100000" },
+        { test: /\.jpg$/, loader: "file-loader" }
+    ]
+  }
+
+  return config;
+}
+
+module.exports = buildConfig(env);
+
